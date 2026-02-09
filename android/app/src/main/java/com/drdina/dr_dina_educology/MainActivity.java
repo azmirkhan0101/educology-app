41d013247da0358496de261228e651be5df29f35
Azmir Khan <azmir.azamkhan@gmail.com>
2026-02-09 15:11:33 +0600

    Initial project setup

---
+++ android/app/src/main/java/com/drdina/dr_dina_educology/MainActivity.java
@@ -0,0 +1,6 @@
+package com.drdina.dr_dina_educology;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Hello"),),
+    );
+  }
+}
+
