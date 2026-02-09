a5c1b5ee02dae9e456f7dfdb7d768b0f070605ae
Azmir Khan <azmir.azamkhan@gmail.com>
2026-02-09 17:09:36 +0600

    project structure setup

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -18,7 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Hello"),),
+      body: Center(child: Text("Hello"),
+      ),
     );
   }
 }
